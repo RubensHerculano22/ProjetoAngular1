3a2358af2f2fba6c034322e697da782c0b68840a
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-26 19:40:35 -0300

    Parte 10

--- src/app/modules/home/componentes/todo-list/todo-list.component.ts
+++ src/app/modules/home/componentes/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 //Interface
 import { TaskList } from '../../model/task-list';
@@ -8,12 +8,12 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit{
+export class TodoListComponent implements DoCheck{
   public taskList: Array<TaskList> = [];
   constructor() { }
 
-  ngOnInit(): void {
-    
+  ngDoCheck(): void {
+    this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked))
   }
 
   public setEmitTaskList(event: string){
@@ -28,5 +28,14 @@ export class TodoListComponent implements OnInit{
     const confirm = window.confirm("Voce deseja realmente Deletar tudo?");
     if(confirm)
       this.taskList = [];
+  }
+
+  public validationInput(event: string, index: number) {
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja Deletar?");
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 }
